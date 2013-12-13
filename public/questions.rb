61ac3587eb775365d779464080b1143899e324ee
Sara Gorecki <sgorecki@gmail.com>
2013-12-13 15:07:32 -0500

    fix missing commas

--- public/questions.rb
+++ public/questions.rb
@@ -1,4 +1,4 @@
-quiz_questions = 
+def quiz_questions
   {:one => 
     {:question => "If you were stranded on a desert planet with one other alien, what would you do?",
       :answers => {:et => "Build a flying bicycle out of trees and ride your way off the planet.", :klingon => "Kill the other alien. You will be the sole survivor.", :thedoctor => "Befriend the alien and bring it back in time with you.", :facehugger => "You're looking out for your own species. Latch on to the other alien and never let go."}
@@ -10,11 +10,11 @@ quiz_questions =
   :three =>
     {:question => "What would you do if you were stranded on Earth?",
       :answers => {:navi => "Make a home deep in the rainforest and protect it at all costs.", :thedoctor => "That would be magnificent! Humans are such interesting creatures.", :et => "Try to get back home. Maybe try calling?", :facehugger => "That's practically a feast! All those humans to attack."}
-    }
+    },
   :four =>
     {:question => "How are you with plants?",
       :answers => {:moya => "Do you mean sentient plants? Because I've been friends with those.", :navi => "All of nature is valuable.", :facehugger => "They're completely useless. I have no need for them.", :yoda => "Everything is connected. You can feel the energy from all living things."}
-    }
+    },
   :five =>
     {:question => "What is your biggest strength?",
       :answers => {:klingon => "Battle. I am a great warrior.", :superman => "That would probably be my strength. And being indestructable.", :moya => "Getting people and things where they need to go.", :yoda => "Being attuned to the universe."}
@@ -26,9 +26,10 @@ quiz_questions =
   :seven =>
     {:question => "What is your favorite food?",
       :answers => {:facehugger => "That's a tough one. Probably humans.", :et => "Reese's Pieces", :superman => "Beef Bourguignon with Ketchup", :klingon => "Rokeg blood pie"}
-    }
+    },
   :eight =>
     {:question => "Who do you normally travel with?",
       :answers => {:moya => "A whole group of diverse aliens. I try to keep them safe.", :navi => "I don't usually travel far from home. If I do, I stick with those I know well.", :thedoctor => "If I meet someone who I find interesting, I'll bring them along with me.", :yoda => "Most of my kind are gone. I try to stay with those who have the same philosophy as me."}
     }
   }
+end
